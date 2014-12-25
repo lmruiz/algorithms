b02a96846aeca270c6528420d0f3b12402da4cad
lmruiz <luis.ruiz.pichardo@gmail.com>
2014-12-25 14:53:21 -0600

    Added Tests for LinkedList Manager

--- SingleLinkedList/test/com/innovarit/algorithms/simplelinkedlists/LinkedListManagerTest.java
+++ SingleLinkedList/test/com/innovarit/algorithms/simplelinkedlists/LinkedListManagerTest.java
@@ -11,5 +11,24 @@ public class LinkedListManagerTest {
 		LinkedListManager linkedListManager = new LinkedListManager();
 		assertEquals(0,linkedListManager.listLength(null));
 	}
-
+	
+	@Test
+	public void lengthIncrease(){
+		LinkedListManager linkedListManager = new LinkedListManager();
+		assertEquals(0,linkedListManager.listLength(null));
+		
+		ListNode headNode = null;
+		ListNode nodeToInsert = new ListNode(1);
+		headNode=linkedListManager.insertInLinkedList(headNode, nodeToInsert, 1);
+		assertEquals(1,linkedListManager.listLength(headNode));
+		
+		
+		ListNode nodeToInsert2 = new ListNode(2);
+		headNode=linkedListManager.insertInLinkedList(headNode, nodeToInsert2, 1);
+		assertEquals(2,linkedListManager.listLength(headNode));
+		
+		ListNode nodeToInsert3 = new ListNode(3);
+		headNode=linkedListManager.insertInLinkedList(headNode, nodeToInsert3, 1);
+		assertEquals(3,linkedListManager.listLength(headNode));
+	}
 }
